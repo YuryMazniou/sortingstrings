fef8da7036459a05d05ab60d37d66f5312e33a25
YuryMazniou <tantr@tut.by>
2019-09-27 13:08:40 +0300

    add a little bit  code

--- src/main/java/by/gwtandgrid/task/util/SortingUtil.java
+++ src/main/java/by/gwtandgrid/task/util/SortingUtil.java
@@ -1,6 +1,9 @@
 package by.gwtandgrid.task.util;
 
+import com.google.gwt.regexp.shared.MatchResult;
+import com.google.gwt.regexp.shared.RegExp;
 import com.google.gwt.user.client.ui.Grid;
+import java.math.BigDecimal;
 
 public class SortingUtil {
     private static int height =0;
@@ -17,5 +20,34 @@ public class SortingUtil {
         }
 
         return null;
+    }
+
+    public static int compareLine(String first,String second){
+        RegExp reg = RegExp.compile("\\d+|\\d+\\.\\d+");
+        MatchResult matcherF = reg.exec(first);
+        MatchResult matcherS = reg.exec(second);
+        boolean result=matcherF!=null||matcherS!=null;
+        if(result){
+            BigDecimal f=new BigDecimal(first);
+            BigDecimal s=new BigDecimal(second);
+            return first.compareTo(second);
+        }
+        else {
+            if(matcherF==null&&matcherS!=null){
+                return -1;
+            }
+            else {
+                if(matcherF!=null&&matcherS==null){
+                    return 1;
+                }
+            }
+        }
+        return first.compareTo(second);
+    }
+
+    public static void main(String[] args) {
+        BigDecimal first=new BigDecimal("2.2");
+        BigDecimal second=new BigDecimal("2.21");
+        System.out.println(false||false);
     }
 }
